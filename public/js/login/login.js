804a893890f2394353f31857bbbfcce4382ab0f9
wo_xiaofan <woxiaofan165@gmail.com>
2016-12-21 11:13:17 +0800

    loading页跳转bug修复

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -1,11 +1,10 @@
 /**
  * Created by Administrator on 2016/12/9.
  */
-$(function () {
 
-    $('img').css('opacity',0).css('transition','2s');
-    $('p').css('opacity',0).css('transition','2s');
-   setTimeout(function () {
-       location.href = "./indexPage/indexPage.html";
-   },2000);
-});
+setTimeout(function(){
+    document.getElementById("loading").style.opacity = 0;
+    setTimeout(function () {
+        location.href = './indexPage/indexPage.html';
+    },2000)
+}, 0);
